e6ce5dff1c27863c4b9808ccbc0436d16d2a3384
Markos Rafael <escalante.markos18@gmail.com>
2021-01-14 13:10:33 -0800

    Create gitignore file to ignore node_modules to be uploaded. Create employeedb.sql file to begin our database and transer that information to mysql software application. Create server.js file to instantiate our npm third party packages.

---
+++ employeedb.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+CREATE database employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE employee (
+  id INT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL, 
+  manager_id INT NULL, 
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE department (
+  id INT NOT NULL,
+  department_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee_role (
+  id INT NOT NULL,
+  role_title VARCHAR(30) NOT NULL, 
+  salary DECIMAL (6,2),
+  department_id INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+SELECT * FROM employee;
+select * from department;
+select * from employee_role;
